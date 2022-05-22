9cde977a10944304b7c87e79d370648c32693731
muhammadmb <muhammadmagdyje@gmail.com>
2022-05-22 03:12:21 +0200

    Add Repositories
    - IGenericRepository, IPlayer, IClubRepository, INationalTeamRepository

---
+++ Football Players API/Repositories/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Football_Players_API.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(Guid id);
+
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ Football Players API/Repositories/INationalTeamRepository.cs
@@ -0,0 +1,8 @@
+﻿using Football_Players_API.Entities;
+
+namespace Football_Players_API.Repositories
+{
+    public interface INationalTeamRepository: IGenericRepository<NationalTeam>
+    {
+    }
+}
